a984c2e1af5a1f7bc7d6e197452253471b4d3417
Fildza Hadyan <hadyanf@gmail.com>
2024-08-01 21:24:36 +0700

    Mini Project Software Engineering

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-/*ini JavaScript*/
+
+function validateForm() {
+    const name= document.forms["message-form"]["full-name"].value;
+    const email= document.forms["message-form"]["email"].value;
+    const password= document.forms["message-form"]["password"].value;
+    
+    if (name == "" || email == "" || password == "") {
+    alert("Tidak boleh kosong");
+    return false;
+    }
+    
+    setSenderUI(name, email, password);
+    
+    return false;
+    }
+    
+    function setSenderUI(name, email, password) {
+    document.getElementByID("sender-full-name").innerHTML = name;
+    document.getElementByID("sender-email").innerHTML = email;
+    document.getElementByID("sender-password").innerHTML = password;
+    }
